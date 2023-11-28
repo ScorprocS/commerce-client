9d10872945e5024243e30d076df34be81351add4
Alex <a.blanc100@laposte.net>
2023-11-28 13:41:34 +0100

    Fix controller advice

--- src/main/java/lu/sfeir/commerce/client/controller/ControllerExceptionHandler.java
+++ src/main/java/lu/sfeir/commerce/client/controller/ControllerExceptionHandler.java
@@ -1,39 +1,33 @@
 package lu.sfeir.commerce.client.controller;
 
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import lu.sfeir.commerce.client.clientapi.dto.ErrorResponseDto;
 import lu.sfeir.commerce.client.exception.EntityNotFoundException;
 import lu.sfeir.commerce.client.exception.InvalidParameterException;
 
-
 @ControllerAdvice
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class ControllerExceptionHandler extends ResponseEntityExceptionHandler{
-	
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
 	@ExceptionHandler(value = { EntityNotFoundException.class })
-	//@ResponseStatus(value = HttpStatus.NOT_FOUND)
-	public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(EntityNotFoundException ex, WebRequest request) {
-		return ResponseEntity.badRequest().body(ErrorResponseDto.builder().code(404).message(ex.getMessage()).build());
+	public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(EntityNotFoundException ex) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(ErrorResponseDto.builder().code(HttpStatus.NOT_FOUND.value()).message(ex.getMessage()).build());
 	}
-	
+
 	@ExceptionHandler(value = { InvalidParameterException.class })
-	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
-	public ErrorResponseDto handleBadRequestException(InvalidParameterException ex, WebRequest request) {
-		return ErrorResponseDto.builder().code(400).message(ex.getMessage()).build();
+	public ResponseEntity<ErrorResponseDto> handleBadRequestException(InvalidParameterException ex) {
+		return ResponseEntity.badRequest()
+				.body(ErrorResponseDto.builder().code(HttpStatus.BAD_REQUEST.value()).message(ex.getMessage()).build());
 	}
-	
-	@ExceptionHandler(value = { Exception.class })
-	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-	public ErrorResponseDto handleOtherException(Exception ex, WebRequest request) {
-		return ErrorResponseDto.builder().code(500).message(ex.getMessage()).build();
+
+	@ExceptionHandler( Exception.class )
+	public ResponseEntity<ErrorResponseDto> handleOtherException(Exception ex) {
+		return ResponseEntity.internalServerError().body(ErrorResponseDto.builder()
+				.code(HttpStatus.INTERNAL_SERVER_ERROR.value()).message(ex.getMessage()).build());
 	}
 }
